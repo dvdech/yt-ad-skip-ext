05eeb5aaef33d0bf2da27782274482ae3bcd7031
Dylan DeChiara <dylan@coral.coop>
2023-11-12 19:55:29 -0500

    added entire directory - ext completed.

---
+++ content.js
@@ -0,0 +1,33 @@
+function checkAdStatus() {
+    if (location.hostname === "www.youtube.com") {
+        const adIndicator = document.querySelector(".ytp-ad-text");
+        //const adStatus = adIndicator ? "An ad is playing." : "No ad is playing.";
+        // console.log(adStatus) - use for debugging
+          if (adIndicator) {
+            const skipButton = document.querySelector(".ytp-ad-skip-button-container button");
+            if (skipButton) {
+              skipButton.click();
+            }
+          }
+        }
+}
+
+// check if current url is youtube.com 
+function isYouTubeTab(url) {
+    return url.includes("youtube.com");
+  }
+  
+  // Function to start or stop polling based on tab visibility
+  function handleTabVisibilityChange() {
+    if (document.visibilityState === 'visible' && isYouTubeTab(window.location.href)) {
+      // Run the check periodically (every 2 seconds in this example)
+      intervalId = setInterval(checkAdStatus, 2000);
+    } else {
+      // Stop the polling if the tab is not visible or not on YouTube
+      clearInterval(intervalId);
+    }
+  }
+  
+  // Initial check and set up event listener for tab visibility changes
+  handleTabVisibilityChange();
+  document.addEventListener('visibilitychange', handleTabVisibilityChange);
